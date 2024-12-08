807add493cbad7ee2ffa2eca96dac478dd5d9343
RaviMauryaHootowl <ravimaurya027@gmail.com>
2024-12-08 07:03:21 +0530

    Adding feature for plot selling & holdings

---
+++ src/pages/BuyFromPlotPage/BuyFromPlotPage.jsx
@@ -0,0 +1,198 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Modal from "react-modal";
+import styles from "./BuyFromPlotPage.module.css";
+import { useSmartEstateContext } from "../../Context/SmartEstateContext";
+
+const BuyFromPlotPage = () => {
+  
+  const id = useParams();
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [buyPercentQuantity, setBuyPercentQuantity] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [plotName, setPlotName] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [percentageDistributed, setBuyPercentageDistributed] = useState();
+  const [holderData, setHolderData] = useState([]);
+
+  const {
+    fetchPlotById,
+    fetchAllStocksForPlot,
+    buyStocks,
+  } = useSmartEstateContext();
+
+  const fetchPlotDetails = async() => {
+    console.log(id.pid);
+    const plotdata = await fetchPlotById(id.pid);
+    const _holderData = await fetchAllStocksForPlot(id.pid);
+    setHolderData(_holderData);
+    console.log("=========================")
+    console.log(holderData);
+    // console.log(plotdata);
+    setPlotName(plotdata.name);
+    setOwnerName(plotdata.creatorId.toString());
+    setBuyPercentageDistributed(parseInt(plotdata.availableStocks.toString()) - parseInt(plotdata.totalQuantity.toString()));
+    console.log(plotName);
+    console.log(ownerName);
+  }
+
+  const [holders, setHolders] = useState([
+    {
+      holderName: "ABCDE",
+      sellingPercent: 20,
+      sellingPrice: "1500",
+    },
+    {
+      holderName: "ABCDE",
+      sellingPercent: 20,
+      sellingPrice: "1489",
+    },
+  ]);
+  const [sellings, setSellings] = useState(
+    {
+      plotName: "ABCDEF",
+      percentDistributed: 85.6,
+      numOfInvestors: 8,
+    });
+
+  const buyFromSeller = async () => {
+    // add code to buy a particular share percent from that seller
+  };
+
+  const [selectedIndex, setSelectedIndex] = useState();
+  const handleOpenBuyModal = (index) => {
+    setIsModalOpen(true);
+    setSelectedIndex(index);
+  };
+
+  const handleCloseBuyModal = () => {
+    setIsModalOpen(false);
+  };
+//stockId, target, quantityToBuy, price, plotId, sellQuantity
+  const handleBuyStocks = async() => {
+    const data = await buyStocks(parseInt(holderData[selectedIndex].stockId.toString()), holderData[selectedIndex].holderAddress, buyPercentQuantity, parseInt(holderData[selectedIndex].price.toString()), parseInt(holderData[selectedIndex].plotId.toString()), 0);
+    console.log(data);
+  }
+  return (
+    <>
+      <button style={{alignSelf: "end"}} onClick={fetchPlotDetails}>.</button>
+      <div className={styles.studentDashboardContainer}>
+        <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseBuyModal}
+        contentLabel="Buy stocks"
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        <div className={styles.modalContainer}>
+          <button className={styles.closeButton} onClick={handleCloseBuyModal}>
+            {/* <CloseIcon /> */}x
+          </button>
+          <h2
+            className={styles.heading}
+            style={{
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
+            Details
+          </h2>
+
+          <div className={styles.formBox}>
+            <div className={styles.inputGroup}>
+              <input
+                className={`${styles.input}`}
+                style={{
+                  resize: "none",
+                }}
+                type="number"
+                placeholder="Percentage to buy"
+                onChange={(e) => setBuyPercentQuantity(e.target.value)}
+                value={buyPercentQuantity}
+              />
+            </div>
+
+            <button
+              className={`${styles.modalIssueBtn}`}
+              onClick={handleBuyStocks}
+            >
+              {isLoading ? "Loading..." : "Buy"}
+            </button>
+          </div>
+        </div>
+      </Modal>
+        <div className={styles.dashboardBox}>
+          <div className={styles.detailsBox}>
+            <span className={styles.detailsHeading}>Property Details</span>
+            <div className={styles.detailsBoxContent}>
+              <span className={styles.key}>Plot ID: </span>
+              <span className={styles.name}>{id.pid}</span>
+              <span className={styles.key}>Plot Name: </span>
+              <span className={styles.name}>{plotName}</span>
+              <span className={styles.key}>Owner Id: </span>
+              <span className={styles.name}>{ownerName}</span>
+              <span className={styles.key}>Percentage Distributed: </span>
+              <span className={styles.name}>{percentageDistributed * -1}%</span>
+              <span className={styles.key}>No of holders: </span>
+              <span className={styles.name}>{holders.length}</span>
+            </div>
+          </div>
+
+          <div className={styles.detailsBox}>
+            <div className={styles.detailsHeading}>
+              <span>Holders ready to sell</span>
+            </div>
+            {holderData.length > 0 ? (
+              <>
+                <div className={styles.docCardHeader}>
+                  <span className={styles.docCardContent}>Holder Name</span>
+                  <span className={styles.docCardContent}>
+                    Selling Percentage
+                  </span>
+                  <span className={styles.docCardContent}>Selling Price</span>
+                </div>
+                {holderData.filter((item) => item.sellable.toString() != "0").map((item, index) => {
+                  return (
+                    <div
+                      className={
+                        index % 2 == 0
+                          ? `${styles.docCard} ${styles.evenDocCard}`
+                          : `${styles.docCard} ${styles.oddDocCard}`
+                      }
+                      onClick={() => {
+                        handleOpenBuyModal(index);
+                      }}
+                    >
+                      <span className={styles.docCardContent}>
+                        {item.userId.toString()}
+                      </span>
+                      <span className={styles.docCardContent}>
+                        {item.sellable.toString()}%
+                      </span>
+                      <span className={styles.docCardContent}>
+                        {item.price.toString()}
+                      </span>
+                    </div>
+                  );
+                })}
+              </>
+            ) : (
+              <span className={styles.emptyListMessage}>No holdings found</span>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BuyFromPlotPage;
---
+++ src/pages/Plots/Plots.jsx
@@ -0,0 +1,352 @@
+import React from "react";
+import styles from "./Plots.module.css";
+import { useNavigate } from "react-router-dom";
+import { useCallback, useState, useEffect, useRef } from "react";
+import Modal from "react-modal";
+// import CloseIcon from "@mui/icons-material/Close";
+import MoonLoader from "react-spinners/MoonLoader";
+import { useSmartEstateContext } from "../../Context/SmartEstateContext";
+import { useAuth } from "@arcana/auth-react";
+
+const Plots = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const auth = useAuth();
+  const {
+    fetchAllPlots,
+  } = useSmartEstateContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const _data = [];
+  const [stockId, setStockId] = useState();
+  const [address, setAddress] = useState("");
+  const [buyQuantity, setBuyQuantity] = useState();
+  const [buyPrice, setBuyPrice] = useState();
+  const [plotId, setPlotId] = useState();
+  const [sellQuantity, setSellQuantity] = useState();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen2, setModalIsOpen2] = useState(false);
+
+  const fetchData = async() => {
+    console.log("Fetching!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    const _data = await fetchAllPlots();
+    console.log(_data);
+    setData(_data);
+  }
+
+  useEffect(() => {
+    if(auth.isLoggedIn && auth.user){
+      fetchData();
+    }
+  }, [auth]);
+
+  const handleBuyButtonClick = () => {
+
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+  function closeModal2() {
+    setModalIsOpen2(false);
+  }
+
+  const openModal = async (e) => {
+    e.preventDefault();
+    setModalIsOpen(true);
+  };
+  const openModal2 = async (e) => {
+    e.preventDefault();
+    setModalIsOpen2(true);
+  };
+
+  const bigNumberToDecimal = (num) => {
+    return parseInt(num._hex, 16);
+  }
+
+  return (
+    <>
+      <div className={styles.detailsBox}>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Buy stocks"
+          style={{
+            content: {
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+            },
+          }}
+        >
+          <div className={styles.modalContainer}>
+            <button className={styles.closeButton} onClick={closeModal}>
+              {/* <CloseIcon /> */}
+            </button>
+            <h2
+              className={styles.heading}
+              style={{
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              Details
+            </h2>
+
+            <form className={styles.formBox}>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Stock ID"
+                  onChange={(e) => setStockId(e.target.value)}
+                  value={stockId}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="text"
+                  placeholder="Creator's address"
+                  onChange={(e) => setAddress(e.target.value)}
+                  value={address}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Quantity to buy"
+                  onChange={(e) => setBuyQuantity(e.target.value)}
+                  value={buyQuantity}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Price"
+                  onChange={(e) => setBuyPrice(e.target.value)}
+                  value={buyPrice}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Plot ID"
+                  onChange={(e) => setPlotId(e.target.value)}
+                  value={plotId}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Sell Quantity"
+                  onChange={(e) => setSellQuantity(e.target.value)}
+                  value={sellQuantity}
+                />
+              </div>
+
+              <button
+                className={`${styles.modalIssueBtn}`}
+                onClick={handleBuyButtonClick}
+              >
+                {isLoading ? (
+                  <MoonLoader
+                    className={styles.loader}
+                    color="white"
+                    size={20}
+                  />
+                ) : (
+                  "Buy"
+                )}
+              </button>
+            </form>
+          </div>
+        </Modal>
+        <span className={styles.heading}>All Plots</span>
+        <div className={styles.eventPageBody}>
+          {/* <div className={styles.searchEventsContainer}>
+          <div className={`${styles.inputContainer}`}>
+            <div className={styles.searchEventsTitle}>
+              Search Plot by{" "}
+              <select
+                onChange={(e) => {
+                  // setSearchId(e.target.value);
+                  // handleChangeSearchId2(e);
+                }}
+                className={`${styles.input}`}
+              >
+                {/* <option key={0}></option> */}
+          {/* {detailsList.map((value, id) => {
+                  return <option key={id}>{value}</option>;
+                })} 
+              </select>
+            </div>
+          </div>
+          <div className={`${styles.inputContainer2}`}>
+            {`${searchId}` == "docName" ||
+            `${searchId}` == "department" ||
+            `${searchId}` == "reqType" ? (
+              <select
+                onChange={(e) => setSearchInput(e.target.value)}
+                className={`${styles.input2}`}
+              >
+                {/* <option key={0}></option> 
+                {searchIdList.map((value, id) => {
+                  return <option key={id}>{value}</option>;
+                })}
+              </select>
+            ) : (
+              <input
+                className={styles.eventSearchInput}
+                type="text"
+                placeholder={`Search ${searchId}`}
+                value={searchInput}
+                onChange={(e) => {
+                  setSearchInput(e.target.value);
+                }}
+              />
+            )}
+
+            <button
+              className={styles.requestFileBtn}
+              onClick={(e) => handleSearchInput(e)}
+            >
+              Search
+            </button>
+          </div> 
+        </div>*/}
+          <div className={styles.exploreEventsContainer}>
+            <div className={styles.eventsListGrid}>
+              {data.length !== 0 && data.map((request, id) => {
+                console.log(request)
+                return (
+                  
+                  <div id={id} className={styles.eventBox}>
+                    <div className={styles.eventName}>
+                      <span>
+                        Plot ID:{" "}
+                        <span className={styles.values}>{bigNumberToDecimal(request.id)}</span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Creator Id:{" "}
+                        <span className={styles.values}>
+                          {bigNumberToDecimal(request.creatorId)}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Plot name:{" "}
+                        <span className={styles.values}>{request.name}</span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Real Address:{" "}
+                        <span className={styles.values}>{request.realAdd}</span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Coordinates:{" "}
+                        <span className={styles.values}>
+                          {request.xCor} {request.yCor}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Total Quantity:{" "}
+                        <span className={styles.values}>
+                          {bigNumberToDecimal(request.totalQuantity)}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Buyable Quantity:{" "}
+                        <span className={styles.values}>
+                          {bigNumberToDecimal(request.availableStocks)}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Price:{" "}
+                        <span className={styles.values}>{bigNumberToDecimal(request.price)}</span>
+                      </span>
+                    </div>
+                    <div className={styles.eventName}>
+                      <span>
+                        Rent Amount:{" "}
+                        <span className={styles.values}>
+                          {bigNumberToDecimal(request.rentAmount)}
+                          {/* {ethers.utils.formatEther( request.rentAmount )} */}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.buttonBox}>
+                      <div>
+                        <button
+                          onClick={(e) => {
+                            // openModal(e);
+                            // set_ReqId(request.reqId);
+                            navigate(`/buyfrom/${bigNumberToDecimal(request.id)}`)
+                          }}
+                          className={styles.issueBtn}
+                        >
+                          <span>Buy Stocks</span>
+                        </button>
+                      </div>
+
+                      <div>
+                        <button
+                          onClick={(e) => {
+                            openModal2(e);
+                            // set_ReqId(request.reqId);
+                            // set_Email(request.emailId);
+                          }}
+                          className={styles.rejectBtn}
+                        >
+                          <span>Rent plot</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Plots;
